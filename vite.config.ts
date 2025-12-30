2792e2a788a24b924c0bbdd3e9e547918d2b8e7d
Illia Popov <ilyapopov9406@gmail.com>
2025-12-30 15:40:20 +0200

    fixed deploy

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,5 @@ export default defineConfig({
         environment: 'jsdom',
         setupFiles: ['./src/setupTests.ts'],
     },
-    base: "/carousel-showcasing-products"
+    base: "/carousel-showcasing-products/"
 })
